82b1c8297e353c79f53d0b79bd142485d652cf3e
Sajaki <Sajaki9@gmail.com>
2017-09-13 19:27:15 +0200

    German translation

--- language/nl/info_acp_recenttopics.php
+++ language/nl/info_acp_recenttopics.php
@@ -19,6 +19,22 @@ if (empty($lang) || !is_array($lang))
 	$lang = array();
 }
 
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
+
 $lang = array_merge(
 	$lang, array(
 		//forum acp
@@ -46,7 +62,7 @@ $lang = array_merge(
 		'RT_PARENTS'                    => 'Weergeven van hoofdforums',
 		'RT_PARENTS_EXP'                => 'Toon de hoofdforums in de onderwerpregel van de recente onderwerpen.',
 
-		//Instellingen
+		//user instellingen
 		'RT_OVERRIDABLE'                => 'Instellingen waarvoor gebruikerspaneel voorrang heeft',
 		'RT_LOCATION'                   => 'Blok instellingen',
 		'RT_LOCATION_EXP'               => 'Kies plaats van ’Recente onderwerpen’ blok.',
