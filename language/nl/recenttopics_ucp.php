b7b90eb0d2cde593d18140d9364359cfdd3cf1b4
Sajaki <Sajaki9@gmail.com>
2017-09-13 09:52:34 +0200

    nederlandse vertaling

--- language/nl/recenttopics_ucp.php
+++ language/nl/recenttopics_ucp.php
@@ -23,9 +23,9 @@ $lang = array_merge(
 	$lang, array(
 	'RT_ENABLE'              => 'Toon op on index pagina',
 	'RT_LOCATION'            => 'Plaatsinstelling',
-	'RT_LOCATION_EXP'        => 'Stel plaats van \'recente onderwerpen\' blok in.',
-	'RT_SORT_START_TIME'     => 'Sorteer op plaatsingstijd',
-	'RT_SORT_START_TIME_EXP' => 'dus niet op laatste reactie',
+	'RT_LOCATION_EXP'        => 'Stel plaats van ’recente onderwerpen’ blok in.',
+	'RT_SORT_START_TIME'     => 'Sorteer op onderwerptijdstip',
+	'RT_SORT_START_TIME_EXP' => 'Sorteer op onderwerptijdstip, niet op tijdstip laatste reactie',
 	'RT_UNREAD_ONLY'         => 'Alleen ongelezen onderwerpen weergeven',
 	'RT_TOP'                 => 'Toon boven',
 	'RT_BOTTOM'              => 'Toon beneden',
