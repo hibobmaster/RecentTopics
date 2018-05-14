05dcc8a18c20981ef237c3f0f72447d446f1a1ec
Sajaki <Sajaki9@gmail.com>
2018-05-14 23:11:53 +0200

    dev 2.2.8

--- acp/recenttopics_module.php
+++ acp/recenttopics_module.php
@@ -45,7 +45,7 @@ class recenttopics_module extends admin
 		$ext_meta_manager = $ext_manager->create_extension_metadata_manager('paybas/recenttopics', $phpbb_container->get('template'));
 		$meta_data  = $ext_meta_manager->get_metadata();
 		$ext_version  = $meta_data['version'];
-		$versionurl = $meta_data['extra']['version-check']['host'].$meta_data['extra']['version-check']['directory'].'/'.$meta_data['extra']['version-check']['filename'];
+		$versionurl = $meta_data['extra']['version-check']['protocol']. $meta_data['extra']['version-check']['host'].$meta_data['extra']['version-check']['directory'].'/'.$meta_data['extra']['version-check']['filename'];
 		$latest_version  = $this->version_check($versionurl, $request->variable('versioncheck_force', false));
 
 		if ($request->is_set_post('submit'))
